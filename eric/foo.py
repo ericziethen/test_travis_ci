e6be30fa1a86b217e00c539d7839b4a0bf923770
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-05-27 09:34:55 +1000

    Cache pip, more failed tests

--- eric/foo.py
+++ eric/foo.py
@@ -13,3 +13,15 @@ def untested_function():
 
 def another_untested_function():
     return 'I\'m not tested'
+
+
+def yet_another_untested_function():
+    for x in range(100):
+        print('Test me Please')
+    return 'I\'m not tested as well'
+
+
+def and_yet_another_untested_function():
+    for x in range(100):
+        print('Test me Please, Please')
+    return 'I\'m not tested as well, are you serious?'

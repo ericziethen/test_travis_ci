a25c6cf32169eab84b4b7521abcdc9b17084ab84
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-05-27 11:38:46 +1000

    Add Linux

---
+++ dev/run_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+PROJ_MAIN_DIR=$BASEDIR/..
+
+pushd $PROJ_MAIN_DIR
+
+PACKAGE_ROOT=eric
+
+export PYTHONPATH=${PYTHONPATH:+${PYTHONPATH}:}$PACKAGE_ROOT
+
+# Test directories are specified in Pytest.ini
+pytest --cov-report term --cov=$PACKAGE_ROOT
+pytest_rcode=$?
+if [[ $pytest_rcode -eq  0 ]];
+then
+    echo "*** No Issues Found"
+else
+    echo "*** Some Issues Found"
+
+fi
+
+popd
+
+exit $pytest_rcode

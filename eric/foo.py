e679e41d3eaa0c6a6ac7a8b13e263f84a045c679
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-05-28 09:42:27 +1000

    misc

--- eric/foo.py
+++ eric/foo.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 
+"""Test Module."""  # pylint: disable=blacklisted-name
+
+
 VERSION = '1.5.dev'
 
 
-def get_version():
+def get_version() -> str:
+    """Comment on this function."""
     return VERSION
 
 
-def untested_function():
+def untested_function() -> str:
+    """Comment on this function."""
     return 'I\'m not tested'
 
 
-def another_untested_function():
+def another_untested_function() -> str:
+    """Comment on this function."""
     return 'I\'m not tested'
 
 
-def yet_another_untested_function():
+def yet_another_untested_function() -> str:
     """Another comment."""
-    for x in range(100):
-        print('Test me Please')
+    for var in range(100):
+        print('Test me Please', var)
     return 'I\'m not tested as well'
 
 
-def and_yet_another_untested_function():
+def and_yet_another_untested_function() -> str:
     """Complain about not being tested."""
-    for x in range(100):
-        print('Test me Please, Please')
+    for var in range(100):
+        print('Test me Please, Please', var)
     return 'I\'m not tested as well, are you serious?'

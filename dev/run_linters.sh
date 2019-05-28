4fa8dfd5c52bf71b78b14b13f45cc28ce26ac81d
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-05-28 15:37:33 +1000

    misc

--- dev/run_linters.sh
+++ dev/run_linters.sh
@@ -35,11 +35,11 @@ ERROR_FOUND="false"
 ERROR_LIST=''
 
 echo "### Start Linting ###"
-run_linter "Bandit"        "./$LINTER_DIR/RunBandit.sh"
-run_linter "MyPy"          "./$LINTER_DIR/RunMyPy.sh"
-run_linter "Pycodestyle"   "./$LINTER_DIR/RunPycodestyle.sh"
-run_linter "Pydocstyle"    "./$LINTER_DIR/RunPydocstyle.sh"
-run_linter "Pylint"        "./$LINTER_DIR/RunPylint.sh"
+run_linter "Bandit"        "$LINTER_DIR/RunBandit.sh"
+run_linter "MyPy"          "$LINTER_DIR/RunMyPy.sh"
+run_linter "Pycodestyle"   "$LINTER_DIR/RunPycodestyle.sh"
+run_linter "Pydocstyle"    "$LINTER_DIR/RunPydocstyle.sh"
+run_linter "Pylint"        "$LINTER_DIR/RunPylint.sh"
 echo "### Linting finished ###"
 
 echo "ERROR_FOUND: '$ERROR_FOUND'"

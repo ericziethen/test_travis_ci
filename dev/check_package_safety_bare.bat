c3dee6af508f6dc0304bc1b4cb6dbab00daf653f
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-05-31 15:29:52 +1000

    misc

--- dev/check_package_safety_bare.bat
+++ dev/check_package_safety_bare.bat
@@ -3,6 +3,7 @@
 rem the safety module throws a Unicode error on Travis, so we need a base version
 
 safety check --bare
+set return_code=%errorlevel%
 if %return_code% equ 0 (
     echo *** No Issues Found
     exit /B 0

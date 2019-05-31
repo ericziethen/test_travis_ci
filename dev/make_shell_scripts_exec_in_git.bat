15418d8d926cbe8b0c88879c2b2b2368020f36ab
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-05-31 12:44:52 +1000

    Add git stuff

---
+++ dev/make_shell_scripts_exec_in_git.bat
@@ -0,0 +1,19 @@
+@echo off
+
+setlocal
+
+set SCRIPT_DIR=%~dp0
+
+pushd %SCRIPT_DIR%
+
+for /R %%I in ("*.sh") do (
+    echo # Current Permissions for "%%I"
+    git ls-files --stage "%%I"
+    git update-index --chmod=+x "%%I"
+    echo # New Permissions for "%%I"
+)
+
+popd
+
+endlocal
+

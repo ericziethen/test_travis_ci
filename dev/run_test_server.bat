8f2f150df5b883b2f96e9caf76b7f9c084697291
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-06-04 09:11:25 +1000

    Setup Test Server

--- dev/run_test_server.bat
+++ dev/run_test_server.bat
@@ -5,4 +5,27 @@ setlocal
 set PROJ_MAIN_DIR=%~dp0..
 set TEST_SERVER_DIR=%PROJ_MAIN_DIR%\tests\TestServerContent
 
-python -m http.server --directory "%TEST_SERVER_DIR%" --bind localhost 8000
+rem --directory only available since Python 3.7
+set COMMAND=python -m http.server --bind localhost 8000
+
+pushd "%TEST_SERVER_DIR%"
+
+if [%1] == [] (
+    echo No Argument Found, run attached
+    goto run_attached
+) else (
+    echo Argument Found, run detached
+    goto run_detached
+)
+
+:run_attached
+%COMMAND%
+goto end
+
+:run_detached
+start "python_test_server" %COMMAND%
+goto end
+
+:end
+popd
+endlocal
--- dev/run_test_server.sh
+++ dev/run_test_server.sh
@@ -4,4 +4,17 @@ SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_MAIN_DIR=$SCRIPT_PATH/..
 TEST_SERVER_DIR=$PROJ_MAIN_DIR/tests/TestServerContent
 
-python -m http.server --directory "$TEST_SERVER_DIR" --bind localhost 8000
+# --directory only available since Python 3.7
+python -m http.server --bind localhost 8000
+
+pushd "$TEST_SERVER_DIR"
+
+if [ "$1" != "" ]; then
+    echo "Argument Found, run detached"
+    python -m http.server --bind localhost 8000 &
+else
+    echo "No Argument Found, run attached"
+    python -m http.server --bind localhost 8000
+fi
+
+popd

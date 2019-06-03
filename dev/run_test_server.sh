0fac6629803f09f44a8f881659c4d11b3b8e3496
eric <eric@home.com>
2019-06-03 20:23:21 +1000

    musv

--- dev/run_test_server.sh
+++ dev/run_test_server.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_MAIN_DIR=$SCRIPT_PATH/..
-TEST_SERVER_DIR=%PROJ_MAIN_DIR%/tests/TestServerContent
+TEST_SERVER_DIR=$PROJ_MAIN_DIR/tests/TestServerContent
 
 python -m http.server --directory "$TEST_SERVER_DIR" --bind localhost 8000

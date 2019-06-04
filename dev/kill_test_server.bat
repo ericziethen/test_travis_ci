91281d4db41e649717bdcf2dcb4b6ed1225232c3
Eric Ziethen <EricSonke.Ziethen@dieboldnixdorf.com>
2019-06-04 10:40:13 +1000

    misc

--- dev/kill_test_server.bat
+++ dev/kill_test_server.bat
@@ -5,6 +5,8 @@ setlocal
 rem DON'T CHANGE, ALSO USED FOR KILLING
 set SERVER_WINDOW_NAME=python_test_server
 
+tasklist /FI "WindowTitle eq %SERVER_WINDOW_NAME%"
+
 echo Command: 'taskkill /F /FI "WindowTitle eq %SERVER_WINDOW_NAME%"'
 taskkill /F /FI "WindowTitle eq %SERVER_WINDOW_NAME%"
 
